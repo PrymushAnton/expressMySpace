47dbd625d4ecced77f1c83fd4508750bcf2e472d
PrymushAnton <anton.primush123663@gmail.com>
2025-05-18 09:58:04 +0300

    fixed errors with id

--- src/PostApp/post.controller.ts
+++ src/PostApp/post.controller.ts
@@ -7,7 +7,7 @@ async function createPost(req: Request, res: Response){
     const data: CreatePost = req.body
     const userId: number = res.locals.userId
 
-    const result = await postService.createPost(data, userId)
+    const result = await postService.createPost(data, +userId)
 
     res.json(result)
 }
@@ -15,6 +15,7 @@ async function createPost(req: Request, res: Response){
 
 async function updatePost(req: Request, res: Response){
     const data: UpdatePost = req.body
+    data.id = +data.id
 
     const result = await postService.updatePost(data)
 
@@ -25,7 +26,7 @@ async function updatePost(req: Request, res: Response){
 async function deletePost(req: Request, res: Response){
     const {id}: {id: number} = req.body
 
-    const result = await postService.deletePost(id)
+    const result = await postService.deletePost(+id)
 
     res.json(result)
 }
--- src/PostApp/post.router.ts
+++ src/PostApp/post.router.ts
@@ -5,7 +5,7 @@ import postController from "./post.controller";
 const postRouter = Router()
 
 
-postRouter.get("/find-by-user-id/:id", postController.findPostsByUserId)
+postRouter.get("/find-posts-by-user-id/:id", postController.findPostsByUserId)
 postRouter.get("/find-all-posts", postController.findAllPosts)
 postRouter.get("/find-all-tags", postController.findAllTags)
 

f66480432b7adf4c29bf83a63eb096cd5249d40f
Ilia Bulkin <bulkin20088@gmail.com>
2025-06-13 22:13:04 +0300

    fix links

--- src/PostApp/post.repository.ts
+++ src/PostApp/post.repository.ts
@@ -1,283 +1,243 @@
-import {client} from "../client/prismaClient"
-import { CreatePost, UpdatePost } from "./types"
+import { client } from "../client/prismaClient";
+import { CreatePost, UpdatePost } from "./types";
 
-async function createPost(data: CreatePost, userId: number){
+async function createPost(data: CreatePost, userId: number) {
+	const {
+		existingTags = [],
+		newTags = [],
+		images = [],
+		link = [],
+		...otherData
+	} = data;
 
-    const {existingTags, newTags, images, ...otherData} = data
-    try{
-        const post = await client.post.create({
-            data: {
-                ...otherData,
-                userId: userId
-            }
-        })
+	try {
+		const post = await client.post.create({
+			data: {
+				...otherData,
+				userId,
+				link: {
+					create: link.map((url) => ({ url })),
+				},
+			},
+		});
 
-        await client.tag.createMany({
-            data: newTags.map((tag) => {
-                return {name: tag}
-            })
-        })
+		if (newTags.length > 0) {
+			await client.tag.createMany({
+				data: newTags.map((tag) => ({ name: tag })),
+			});
+		}
 
-        const foundTags = await client.tag.findMany({
-            where: {
-                name: {
-                    in: [...existingTags, ...newTags]
-                }
-            }
-        })
+		const foundTags = await client.tag.findMany({
+			where: {
+				name: {
+					in: [...existingTags, ...newTags],
+				},
+			},
+		});
 
+		if (foundTags.length > 0) {
+			await client.tagToPost.createMany({
+				data: foundTags.map((tag) => ({
+					tagId: tag.id,
+					postId: post.id,
+				})),
+			});
+		}
 
-        await client.tagToPost.createMany({
-            data: foundTags.map((tag) => {
-                return {tagId: tag.id, postId: post.id}
-            })
-        })
+		if (images.length > 0) {
+			await client.image.createMany({
+				data: images.map((image) => ({
+					base64: image,
+					postId: post.id,
+				})),
+			});
+		}
 
-        await client.image.createMany({
-            data: images.map((image) => {
-                return {base64: image, postId: post.id}
-            })
-        })
+		const fullPost = await client.post.findUnique({
+			where: { id: post.id },
+			include: {
+				tags: true,
+				images: true,
+				link: true,
+			},
+		});
 
-        const fullPost = await client.post.findUnique({
-            where: {
-                id: post.id
-            },
-            include: {
-                tags: true,
-                images: true
-            }
-        })
-
-        return fullPost
-    } catch (error) {
-        console.log((error as Error).message)
-        return "Помилка при роботі з базою даних"
-    }
+		return fullPost;
+	} catch (error) {
+		console.log("CreatePost error:", (error as Error).message);
+		return "Помилка при роботі з базою даних";
+	}
 }
 
+async function updatePost(data: UpdatePost) {
+	const {
+		existingTags = [],
+		newTags = [],
+		images = [],
+		link = [],
+		id,
+		...otherData
+	} = data;
 
-async function updatePost(data: UpdatePost){
+	try {
+		const post = await client.post.update({
+			where: { id },
+			data: otherData,
+		});
 
-    const {existingTags, newTags, images, id, ...otherData} = data
-    try{
-        const post = await client.post.update({
-            where: {
-                id: id
-            },
-            data: otherData
-        })
+		if (newTags.length > 0) {
+			await client.tag.createMany({
+				data: newTags.map((tag) => ({ name: tag })),
+			});
+		}
 
-        await client.tag.createMany({
-            data: newTags.map((tag) => {
-                return {name: tag}
-            })
-        })
+		const foundTags = await client.tag.findMany({
+			where: {
+				name: {
+					in: [...existingTags, ...newTags],
+				},
+			},
+		});
 
-        const foundTags = await client.tag.findMany({
-            where: {
-                name: {
-                    in: [...existingTags, ...newTags]
-                }
-            }
-        })
+		await client.tagToPost.deleteMany({ where: { postId: post.id } });
 
-        await client.tagToPost.deleteMany({
-            where: {
-                postId: post.id
-            }
-        })
+		if (foundTags.length > 0) {
+			await client.tagToPost.createMany({
+				data: foundTags.map((tag) => ({
+					tagId: tag.id,
+					postId: post.id,
+				})),
+			});
+		}
 
-        await client.tagToPost.createMany({
-            data: foundTags.map((tag) => {
-                return {tagId: tag.id, postId: post.id}
-            })
-        })
+		await client.image.deleteMany({ where: { postId: post.id } });
 
-        await client.image.deleteMany({
-            where: {
-                postId: post.id
-            }
-        })
+		if (images.length > 0) {
+			await client.image.createMany({
+				data: images.map((image) => ({
+					base64: image,
+					postId: post.id,
+				})),
+			});
+		}
 
-        await client.image.createMany({
-            data: images.map((image) => {
-                return {base64: image, postId: post.id}
-            })
-        })
+		await client.link.deleteMany({ where: { postId: post.id } });
 
-        const fullPost = await client.post.findUnique({
-            where: {
-                id: post.id
-            },
-            include: {
-                tags: true,
-                images: true
-            }
-        })
+		if (link.length > 0) {
+			await client.link.createMany({
+				data: link.map((url) => ({ url, postId: post.id })),
+			});
+		}
 
-        return fullPost
-    } catch (error) {
-        console.log((error as Error).message)
-        return "Помилка при роботі з базою даних"
-    }
+		const fullPost = await client.post.findUnique({
+			where: { id: post.id },
+			include: {
+				tags: true,
+				images: true,
+				link: true,
+			},
+		});
+
+		return fullPost;
+	} catch (error) {
+		console.log("UpdatePost error:", (error as Error).message);
+		return "Помилка при роботі з базою даних";
+	}
 }
-
-
-async function deletePost(postId: number){
-
-    try{
-        await client.tagToPost.deleteMany({
-            where: {
-                postId: postId
-            }
-        })
-
-        await client.image.deleteMany({
-            where: {
-                postId: postId
-            }
-        })
-
-        const post = await client.post.delete({
-            where: {
-                id: postId
-            },
-        })
-
-        return post
-    } catch (error) {
-        console.log((error as Error).message)
-        return "Помилка при роботі з базою даних"
-    }
-}
-
-async function findPostsByUserId(userId: number){
-
-    try{
-
-        const posts = await client.post.findMany({
-            where: {
-                userId: userId
-            },
-            include: {
-                tags: {
-                    include: {
-                        tag: true
-                    }
-                },
-                images: true
-
-            }
-        })
-
-        const formattedPosts = posts.map((post) => {
-            return {
-                ...post,
-                tags: post.tags.map((tag) => tag.tag.name),
-                images: post.images.map((image) => image.base64)
-            }
-        })
-
-        return formattedPosts
-    } catch (error) {
-        console.log((error as Error).message)
-        return "Помилка при роботі з базою даних"
-    }
-}
-
-
-
-async function findAllPosts(){
-
-    try{
-        const posts = await client.post.findMany({
-            include: {
-                tags: {
-                    include: {
-                        tag: true
-                    }
-                },
-                images: true
-
-            }
-        })
-
-        const formattedPosts = posts.map((post) => {
-            return {
-                ...post,
-                tags: post.tags.map((tag) => tag.tag.name),
-                images: post.images.map((image) => image.base64)
-            }
-        })
-
-        return formattedPosts
-    } catch (error) {
-        console.log((error as Error).message)
-        return "Помилка при роботі з базою даних"
-    }
-}
-
-
-async function findPostById(id: number){
-
-    try{
-        const post = await client.post.findUnique({
-            where: {
-                id: id
-            },
-            include: {
-                tags: {
-                    include: {
-                        tag: true
-                    }
-                },
-                images: true
-            }
-        })
-
-        if (!post) return "Такого посту не існує"
-
-        const formattedPost = {
-            ...post,
-            tags: post.tags.map((tag) => tag.tag.name),
-            images: post.images.map((image) => image.base64)
-        }
-
-        return formattedPost
-    } catch (error) {
-        console.log((error as Error).message)
-        return "Помилка при роботі з базою даних"
-    }
-}
-
-
-async function findAllTags(){
-
-    try{
-        const tags = await client.tag.findMany({
-            select: {
-                id: true,
-                name: true
-            }
-        })
-
-        return tags
-    } catch (error) {
-        console.log((error as Error).message)
-        return "Помилка при роботі з базою даних"
-    }
-}
-
 
 const userRepository = {
-    createPost: createPost,
-    updatePost: updatePost,
-    deletePost: deletePost,
-    findPostsByUserId: findPostsByUserId,
-    findAllPosts: findAllPosts,
-    findAllTags: findAllTags,
-    findPostById: findPostById
-}
+	createPost,
+	updatePost,
+	deletePost: async function (postId: number) {
+		try {
+			await client.tagToPost.deleteMany({ where: { postId } });
+			await client.image.deleteMany({ where: { postId } });
+			await client.link.deleteMany({ where: { postId } });
+
+			const post = await client.post.delete({ where: { id: postId } });
+			return post;
+		} catch (error) {
+			console.log("DeletePost error:", (error as Error).message);
+			return "Помилка при роботі з базою даних";
+		}
+	},
+	findPostsByUserId: async function (userId: number) {
+		try {
+			const posts = await client.post.findMany({
+				where: { userId },
+				include: {
+					tags: { include: { tag: true } },
+					images: true,
+					link: true,
+				},
+			});
+
+			return posts.map((post) => ({
+				...post,
+				tags: post.tags.map((t) => t.tag.name),
+				images: post.images.map((img) => img.base64),
+				link: post.link.map((l) => l.url),
+			}));
+		} catch (error) {
+			console.log("findPostsByUserId error:", (error as Error).message);
+			return "Помилка при роботі з базою даних";
+		}
+	},
+	findAllPosts: async function () {
+		try {
+			const posts = await client.post.findMany({
+				include: {
+					tags: { include: { tag: true } },
+					images: true,
+					link: true,
+				},
+			});
+
+			return posts.map((post) => ({
+				...post,
+				tags: post.tags.map((t) => t.tag.name),
+				images: post.images.map((img) => img.base64),
+				link: post.link.map((l) => l.url),
+			}));
+		} catch (error) {
+			console.log("findAllPosts error:", (error as Error).message);
+			return "Помилка при роботі з базою даних";
+		}
+	},
+	findPostById: async function (id: number) {
+		try {
+			const post = await client.post.findUnique({
+				where: { id },
+				include: {
+					tags: { include: { tag: true } },
+					images: true,
+					link: true,
+				},
+			});
+
+			if (!post) return "Такого посту не існує";
+
+			return {
+				...post,
+				tags: post.tags.map((t) => t.tag.name),
+				images: post.images.map((img) => img.base64),
+				link: post.link.map((l) => l.url),
+			};
+		} catch (error) {
+			console.log("findPostById error:", (error as Error).message);
+			return "Помилка при роботі з базою даних";
+		}
+	},
+	findAllTags: async function () {
+		try {
+			return await client.tag.findMany({
+				select: { id: true, name: true },
+			});
+		} catch (error) {
+			console.log("findAllTags error:", (error as Error).message);
+			return "Помилка при роботі з базою даних";
+		}
+	},
+};
 
 export default userRepository;
